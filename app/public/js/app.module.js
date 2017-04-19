4a90b548d2b06b65391b7f2d427aac884e597db9
lanefaison2 <faisonusmc@gmail.com>
2017-04-19 16:32:06 -0600

    updates

--- app/public/js/app.module.js
+++ app/public/js/app.module.js
@@ -1,4 +1,4 @@
 (function () {
   'use strict'
-  angular.module('app',['ui.router', 'angularMoment'])
+  angular.module('app',['ui.router', 'angularMoment', 'duScroll'])
 }())
--- app/public/js/pages/home.component.js
+++ app/public/js/pages/home.component.js
@@ -7,13 +7,17 @@
       controller: HomeController
     })
 
-  HomeController.$inject = ['$http']
-  function HomeController($http) {
+  HomeController.$inject = ['$http','$location','$anchorScroll']
+  function HomeController($http,$location,$anchorScroll) {
     const vm = this
 
     vm.$onInit = function () {
       $http.get('/tools').then(function (result) {
         vm.tools = result.data
+      })
+      $http.get('/projects').then(function (result) {
+        vm.projects = result.data
+        console.log(vm.projects)
       })
     }
 
@@ -23,6 +27,11 @@
 
     vm.hoverOut = function () {
       this.hover = false;
+    }
+
+    vm.gotoAbout = function () {
+      $location.hash('about')
+      $anchorScroll
     }
   }
 }())
